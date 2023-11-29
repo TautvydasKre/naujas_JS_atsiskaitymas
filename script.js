46f80095b887b50263620830d2b6d91cf3cd0ba0
Arnas Simaitis <113042490+ArnasSimairis@users.noreply.github.com>
2023-11-29 13:36:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+"use strict";
+
+// pirma uzduotis...
+
+// let skaiciukas;
+
+// for (let i = 0; ; i++) {
+//   skaiciukas = parseInt(
+//     prompt(`Iveskite skaicioka... isrysiu katra pirmaji neigiama... `)
+//   );
+//   if (skaiciukas < 0) {
+//     console.log(skaiciukas);
+//     break;
+//   }
+// }
+
+//  antra uzduotis...
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+//  trecia uzduotis
+
+// for (let i = 2; i < 12; i++) {
+//   console.log(i ** 3);
+// }
+
+// ketvirta uzduotis
+
+// const persons = {
+//   jonas: 30,
+//   petras: 26,
+//   ona: 45,
+//   maryte: 36,
+//   antanas: 77,
+//   "teta zita": 86,
+// };
+
+// for (let vardas in persons) {
+//   const amzius = persons[vardas];
+//   console.log(`Vardas: ${vardas}, Amzius: ${amzius}`);
+// }
+
+// penkta uzduotis
+
+// const pokemonai = {
+//   Pikaciu: "kovos gale 999",
+//   Balbazauras: "kovos gale 875",
+//   Cikorita: "kovos gale 798",
+//   MewTwo: "kovos gale 9999",
+// };
+
+// let pokemonuSK = 0;
+
+// for (let reiksmes in pokemonai) {
+//   console.log(`${reiksmes}: ${pokemonai[reiksmes]}`);
+//   pokemonuSK++;
+// }
+
+// console.log(`yra ovatiekvat reiksmiuuu: ${pokemonuSK}`);
+
+// sesta uzduotis
+
+// const sakinuks = prompt(`Iveskite savo nuostabiausia sakinuka:... `);
+// const zodeliai = sakinuks.split(" ");
+// for (const zodis of zodeliai) {
+//   console.log(zodis);
+// }
+
+// septinta uzduotis
+
+// const sakiniuks = prompt(`Iveskite dar viena savo nuostabiausia sakinuka:... `);
+// for (const raideles of sakiniuks) {
+//   console.log(raideles);
+// }
+// console.log(`Sakinuko ilgis: ${sakiniuks.length}`);
